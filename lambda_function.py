19a583c3b174990280ecdf9bce505fad840d1d60
suvlok <suvanloku@hotmail.com>
2022-12-31 18:10:26 -0500

    completed the serverless lambda function

--- lambda_function.py
+++ lambda_function.py
@@ -2,7 +2,7 @@ import boto3
 import json
 from custom_encoder import CustomEncoder
 import logging
-logger = loggin.getLogger()
+logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
 dynamodbTableName = 'books-db'
@@ -25,22 +25,103 @@ def lambda_handler(event, context):
     if httpMethod == getMethod and path == healthPath:
         response = buildResponse(200)
     elif httpMethod == getMethod and path == bookPath:
-        repsonse = getProduct(event['queryStringParameters']['bookId'])
+        response = getBook(event['queryStringParameters']['bookId'])
     elif httpMethod == getMethod and path == booksPath:
-        response = getProducts()
+        response = getBooks()
     elif httpMethod == postMethod and path == bookPath:
         response = saveBook(json.loads(event['body']))
     elif httpMethod == patchMethod and path == bookPath:
         requestBody = json.loads(event['body'])
-        response = modifyBook(requestBody['booksId'], requestBody['updateKey'], requestBody['updateValue'])
+        response = modifyBook(requestBody['bookId'], requestBody['updateKey'], requestBody['updateValue'])
     elif httpMethod == deleteMethod and path == bookPath:
         requestBody = json.loads(event['body'])
-        response = deleteProduct(requestBody['bookId'])
+        response = deleteBook(requestBody['bookId'])
     else:
         response = buildResponse(404, 'Not Found')
     
     return response
 
+def getBook(bookId):
+    try:
+        response = table.get_item(
+            Key={
+                'bookId': bookId
+            }
+        )
+        if 'Item' in response:
+            return buildResponse(200, response['Item'])
+        else:
+            return buildResponse(404, {'Message': 'BookId: %s not found' % bookId})
+    except:
+        logger.exception('An exception has occured')
+
+def getBooks():
+    try:
+        response = table.scan()
+        result = response['Items']
+
+        while 'LastEvaluatedKey' in response:
+            response = table.scan(ExclusiveStartKey=response['LastEvaluatedKey'])
+            result.extend(response['Items'])
+
+        body = {
+            'books': result
+        }
+
+        return buildResponse(200, body)
+
+    except:
+        logger.exception('An exception has occured')
+
+def saveBook(requestBody):
+    try:
+        table.put_item(Item=requestBody)
+        body = {
+            'Operation': 'SAVE',
+            'Message': 'SUCCESS',
+            'Item': requestBody
+        }
+        return buildResponse(200, body)
+    except:
+       logger.exception('An exception has occured') 
+
+def modifyBook(bookId, updateKey, updateValue):
+    try:
+        response = table.update_item(
+            Key={
+                'bookId': bookId,
+            },
+            UpdateExpression=' set %s = :value' % updateKey,
+            ExpressionAttributeValues={
+                ':value': updateValue
+            },
+            ReturnValues='UPDATED_NEW'
+        )
+        body = {
+            'Operation': 'UPDATE',
+            'Message': 'SUCCESS',
+            'UpdatedAttributes': response
+        }
+        return buildResponse(200, body)
+    except:
+        logger.exception('An exception has occured')
+
+def deleteBook(bookId):
+    try:
+        response = table.delete_item(
+            Key={
+                'bookId':bookId
+            },
+            ReturnValues='ALL_OLD'
+        )
+        body = {
+            'Operation': 'DELETE',
+            'Message':'SUCCESS',
+            'deletedItem': response
+        }
+        return buildResponse(200, body)
+    except:
+        logger.exception('An exception has occured')
 
 def buildResponse(statusCode, body=None):
     response = {
